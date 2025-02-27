3c34351f679ed5027bdf2f988dd2bf4bfcc588e8
Hatice AKGÜL <katrancihatice@icloud.com>
2025-02-28 00:54:06 +0300

    Update project

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,7 @@ import { Appbar } from 'react-native-paper';
 
 const Header = () => {
     return (
-        <Appbar.Header style={{ backgroundColor: '#6200ee' }}>
+        <Appbar.Header style={{ backgroundColor: 'gray' }}>
             <Appbar.BackAction onPress={() => console.log('Geri')} />
             <Appbar.Content title="Ana Sayfa" />
             <Appbar.Action icon="magnify" onPress={() => console.log('Ara')} />
